60c98ce63f8ae04cd33f414bf3be99dd251d9bf3
mikey <michal_maslanka@onet.pl>
2024-09-24 17:33:14 +0200

    screenshot update

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -3,8 +3,8 @@
 CHOSEN=$(printf "Screen\nArea\nColor" | tofi -c "$HOME"/.config/tofi/small-config)
 
 case "$CHOSEN" in
-	"Screen") grim ;;
-	"Area") grim -g "$(slurp)" ;;
-	"Color") hyprpicker -a ;;
+	"Screen") grim - | wl-copy ;;
+	"Area") grim -g "$(slurp)" - | wl-copy ;;
+	"Color") hyprpicker -a | tofi ;;
 	* ) exit 1 ;;
 esac
