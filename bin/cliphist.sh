6050cea62c6eabe4d41241c575d1f392f274376f
mikey <michal_maslanka@onet.pl>
2024-10-23 10:41:43 +0200

    renamed tablet scripts

--- bin/cliphist.sh
+++ bin/cliphist.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-cliphist list | tofi | cliphist decode | wl-copy
+# cliphist list | tofi | cliphist decode | wl-copy
+
+# CLIPHIST=$(cliphist list | sed "/^--/d")
+
+#echo $CLIPHIST
+
+rofi -modi clipboard:~/.config/rofi/modis/cliphits-img -show clipboard
