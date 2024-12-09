64122163afb236ded76170e1e2cf7f6612f6a718
mikey <michal_maslanka@onet.pl>
2024-12-09 20:34:38 +0100

    nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,27 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+vim.cmd.colorscheme("catppuccin-macchiato")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+local a = 5
+
+print(a)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+	vim.highlight.on_yank()
+    end,
+})
+
+
