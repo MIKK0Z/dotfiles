8085fd7bc5fae8646ab063167e0acb31b04ad0b7
mikey <michal_maslanka@onet.pl>
2024-12-15 15:09:13 +0100

    aaaa

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,16 +3,19 @@ vim.g.maplocalleader = " "
 
 require("config.lazy")
 
-vim.opt.clipboard = "unnamedplus"
-
 vim.cmd.colorscheme("catppuccin-macchiato")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 4
 
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking",
